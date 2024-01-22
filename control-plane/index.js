c25d914c989f2ccb4f0e46d55d35814418061874
ryoheimorimoto <ryohei.morimoto@sony.com>
2024-01-22 11:41:06 +0900

    双方向通信および時間計測が可能になった。

--- control-plane/index.js
+++ control-plane/index.js
@@ -1,6 +1,8 @@
+// コントロールプレーンのサーバーコード
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const readline = require('readline');
 
 const app = express();
 const server = http.createServer(app);
@@ -9,12 +11,30 @@ const io = socketIo(server);
 io.on('connection', (socket) => {
     console.log('A worker connected');
 
-    // 例: ワーカーにメッセージを送信
-    socket.emit('message', { data: 'Hello from Control Plane!' });
+    socket.on('error', (error) => {
+        console.error('Socket error:', error);
+    });
 
-    // ワーカーからのメッセージを受け取る
-    socket.on('response', (data) => {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.prompt();
+
+    rl.on('line', (line) => {
+        const startTime = process.hrtime.bigint(); // 送信開始時間
+        socket.emit('message', line, () => {
+            const endTime = process.hrtime.bigint(); // Ack受信時間
+            const durationMs = Number(endTime - startTime) / 1000000; // ナノ秒からミリ秒へ変換
+            console.log(`Ack received. Round trip time: ${durationMs.toFixed(3)} milliseconds`);
+            rl.prompt();
+        });
+    });
+
+    socket.on('response', (data, callback) => {
         console.log('Message from worker:', data);
+        callback();
     });
 });
 
--- worker/index.js
+++ worker/index.js
@@ -1,14 +1,33 @@
+// ワーカーのクライアントコード
 const io = require('socket.io-client');
-const socket = io('http://[コントロールプレーンのIPアドレス]:3000');
+const readline = require('readline');
+const socket = io('http://192.168.10.178:3000');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 socket.on('connect', () => {
     console.log('Connected to Control Plane');
-
-    // コントロールプレーンへのメッセージ送信
-    socket.emit('response', { data: 'Hello from Worker!' });
+    rl.prompt();
 });
 
-// コントロールプレーンからのメッセージを受け取る
-socket.on('message', (data) => {
+socket.on('error', (error) => {
+    console.error('Socket error:', error);
+});
+
+rl.on('line', (line) => {
+    const startTime = process.hrtime.bigint(); // 送信開始時間
+    socket.emit('response', line, () => {
+        const endTime = process.hrtime.bigint(); // Ack受信時間
+        const durationMs = Number(endTime - startTime) / 1000000; // ナノ秒からミリ秒へ変換
+        console.log(`Ack received. Round trip time: ${durationMs.toFixed(3)} milliseconds`);
+        rl.prompt();
+    });
+});
+
+socket.on('message', (data, callback) => {
     console.log('Message from Control Plane:', data);
+    callback(); // Ackを送信
 });
